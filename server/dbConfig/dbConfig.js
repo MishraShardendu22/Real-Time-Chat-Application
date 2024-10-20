2511c65fd48a3c246671235c242ca9df432d8027
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-10-20 15:41:37 +0530

    Database Configured and setup

--- server/dbConfig/dbConfig.js
+++ server/dbConfig/dbConfig.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config();
 const MONGO_URI=process.env.MONGO_URI;
 
 export const dbConfig = async () => {
     try {
+        // testing ignore
+        // console.log("Connecting to the database:", MONGO_URI);
+        
         const connect = await mongoose.connect(MONGO_URI)
-        console.log(connect) // testing ignore
+        // console.log(connect) 
     } catch (err) {
         console.log("There was an error connecting to the database");
         console.log(err);
