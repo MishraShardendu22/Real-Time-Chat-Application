897c22112b8cb4ba48df5017225e1e4f76d23ebc
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-10-20 16:07:01 +0530

    Routes Initialised

---
+++ server/route/message.route.js
@@ -0,0 +1,13 @@
+import Conversation from '../model/conversation.model.js';
+import Message from '../model/message.model.js';
+import User from "../model/user.model.js"
+import express from 'express';
+
+const router = express.Router();
+
+// main routes go here : 
+
+
+// till here 
+
+export default router;
