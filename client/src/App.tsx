22840026225d475a9f7555307fc571bc8b92594f
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-10-21 03:05:54 +0530

    Login Page Done and Tested

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,34 @@
-
-
+import { Routes, Route } from "react-router-dom";
+import Login from "./login/login.tsx";
+import Register from "./register/register.tsx";
+import { AuthContextProvider } from "./context/authContext.tsx";
+import { Toaster } from "sonner";
+import { Boxes } from "./components/ui/background-boxes.tsx";
 
 const App = () => {
   return (
-    <div>
-    </div>
-  )
+    <AuthContextProvider>
+      {/* routes for login and register start here*/}
+      <div className="h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+        <div className="absolute inset-0 w-full h-full z-20 [mask-image:radial-gradient(transparent,grey)] pointer-events-none" />
+        <Boxes />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+      {/* routes for login and register end here*/}
+
+{/* -------------------------------------------------------------------------------------------------------------------------------------------- */}
+
+      {/* routes for the rest of the app start here*/}
+      <Routes>
+
+      </Routes>
+      {/* routes for the rest of the app end here*/}
+      <Toaster />
+    </AuthContextProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/register/register.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Boxes } from "../components/ui/background-boxes";
+
+const Register = () => {
+  return (
+    <h2>Register</h2>
+  );
+}
+
+export default Register;
