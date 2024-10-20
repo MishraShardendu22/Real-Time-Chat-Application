9699693129f72f970399d8ec342d7367d494079b
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-10-21 01:35:36 +0530

    User Routes Models and Cntrollers compelted and tested uisng Postman

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,13 +1,13 @@
-import Conversation from '../model/conversation.model.js';
-import Message from '../model/message.model.js';
-import User from "../model/user.model.js"
 import express from 'express';
+import { isLogin } from "../middleware/isLogin.js"
+import { getUserBySearch,getCurrentChats } from "../controller/user.controller.js"
 
 const router = express.Router();
 
 // main routes go here : 
 
-
+router.get('/search',isLogin,getUserBySearch);
+router.get('/currentChats',isLogin,getCurrentChats)
 
 // till here 
 
