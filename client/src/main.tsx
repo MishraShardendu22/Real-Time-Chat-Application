6ace0e2b9ed0cdced77d7cda2c99809d16671417
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-10-21 04:04:51 +0530

    Completed Login Register and Optimised the Background Boxes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,10 @@
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import { StrictMode } from 'react'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
 )
