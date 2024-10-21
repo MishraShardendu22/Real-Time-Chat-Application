17ddfa44db9a3ca5c442fbc3a8cebe2ad424cad1
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-10-21 11:08:03 +0530

    Verifying User

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Register from "./register/register.tsx";
 import { AuthContextProvider } from "./context/authContext.tsx";
 import { Toaster } from "sonner";
 import Home from "./home/home.tsx";
+import { VerifyUser } from "./utils/VerifyUser.tsx";
 
 const App = () => {
   return (
@@ -11,7 +12,10 @@ const App = () => {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route element={<VerifyUser />}>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Register />} />
+        </Route>
       </Routes>
       <Toaster />
     </AuthContextProvider>
